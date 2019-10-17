b3092edae2ec8856b70b481922f6b93218ad7e7f
mengya <fengming19981221@163.com>
2019-10-17 11:54:57 +0800

    auto第四个笔记的添加

--- everydaypush.py
+++ everydaypush.py
@@ -1,7 +1,10 @@
 import os
 
+print("这次拥护啥:",end="")
+reason = input()
+
 os.system('git add .')
-os.system('git commit -m\"python auto push\"')
+os.system('git commit -m\"auto'+reason+'\"')
 os.system('git push -u origin master')
 
 
